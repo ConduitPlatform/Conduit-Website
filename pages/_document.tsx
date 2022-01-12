c5ce846eecaeadfe8890661a0e5f5e3ac1e6c52c
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-01-12 16:49:38 +0200

    fix(file-extensions)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,8 +13,8 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;1,100&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {this.props.emotionStyleTags}
