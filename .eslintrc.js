9e96782c9553c91d77b766820357880c579ef928
ssellas <sellas.spy@gmail.com>
2022-01-12 16:25:25 +0200

    add: eslint - prettier

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    "no-eval": "error",
+    "no-console": "warn",
+    "react/prop-types": 0,
+    "import/first": "error",
+    "max-len": ["error", 100],
+    "no-case-declarations": 0,
+    "no-use-before-define": "off",
+    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "@typescript-eslint/no-var-requires": [0],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-use-before-define": ["warn"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@next/next/no-document-import-in-page": "off",
+    "@next/next/no-img-element": "off"
+  }
+};
+
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2021,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "extends": [
+    "next/core-web-vitals",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "rules": {
+    "no-eval": "error",
+    "no-console": "warn",
+    "react/prop-types": 0,
+    "import/first": "error",
+    "max-len": ["error", 100],
+    "no-case-declarations": 0,
+    "no-use-before-define": "off",
+    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-use-before-define": ["warn"],
+  }
+};
+
+
