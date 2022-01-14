063fd74f81539b4c2faf845819db38feda512974
jasonkatsoutas <jasonskate33@gmail.com>
2022-01-14 11:38:28 +0200

    fix: background linear gradient

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -3,10 +3,14 @@ import Landing1 from '../src/components/structural/LandingPage/LandingPageUpper'
 import Landing2 from '../src/components/structural/LandingPage/LandingPageLower';
 import { Box, Container, Divider, Typography } from '@mui/material';
 import LandingPageUsers from '../src/components/structural/LandingPage/LandingPageUsers';
+import theme from '../src/theme';
 
 export default function Index() {
   return (
-    <Box sx={{ background: 'linear-gradient(0deg, rgba(38,40,64,1) 27%, rgba(31,32,52,1) 89%)' }}>
+    <Box
+      sx={{
+        background: `linear-gradient(0deg, ${theme.palette.background.default} 27%, ${theme.palette.background.paper} 89%)`,
+      }}>
       <Container maxWidth={'xl'} sx={{ pt: [4, 8, 12] }}>
         <Landing1 />
         <Divider sx={{ my: 8 }}>
