62f28ac2d85ee21e2cfd12264f8e7e3e95e0d949
dasno <ep.giannakopoulos@gmail.com>
2022-01-14 16:13:16 +0200

    add: FAQ page add: Accordion component

---
+++ www/src/components/custom/Accordion.tsx
@@ -0,0 +1,71 @@
+import React, { useState, SyntheticEvent, FC } from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, { AccordionSummaryProps } from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
+interface Props {
+  data: { title: string; content: string }[];
+}
+
+const CustomAccordion: FC<Props> = ({ data }) => {
+  const [expanded, setExpanded] = useState<number | false>(false);
+
+  const handleChange = (panelIndex: number) => (event: SyntheticEvent, newExpanded: boolean) => {
+    setExpanded(newExpanded ? panelIndex : false);
+  };
+
+  return (
+    <Box>
+      {data.map((item, index) => (
+        <Accordion expanded={expanded === index} onChange={handleChange(index)} key={index}>
+          <AccordionSummary>
+            <Typography>{item.title}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{item.content}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </Box>
+  );
+};
+
+export default CustomAccordion;
