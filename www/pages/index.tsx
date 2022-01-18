80a3ad8191d11f2c541ecbfb844d65caab7a3b0c
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-01-18 13:22:12 +0200

    fix(blog): delete all blog related code from this branch

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -24,7 +24,6 @@ export default function Index() {
       <FadeOnScrollSection>
         <Roadmap />
       </FadeOnScrollSection>
-
       <FadeOnScrollSection>
         <AboutUsSection />
       </FadeOnScrollSection>
