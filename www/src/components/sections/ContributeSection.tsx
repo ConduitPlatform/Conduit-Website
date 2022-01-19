9e45cbbd40cbcc8be7a31a059e0ffe1d66ccb72b
jasonkatsoutas <jasonskate33@gmail.com>
2022-01-19 18:34:05 +0200

    feat: contribute section

--- www/src/components/sections/ContributeSection.tsx
+++ www/src/components/sections/ContributeSection.tsx
@@ -2,11 +2,12 @@ import { FC } from 'react';
 import { Box, Button, Typography } from '@mui/material';
 import * as React from 'react';
 import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import Link from '../../Link';
 const ContributeSection: FC = () => {
   return (
     <Box mt={[8, 16, 20]}>
       <Typography gutterBottom align={'center'} variant={'h3'}>
-        <strong>Join the conduit Community</strong>
+        <strong>Join the Conduit Community</strong>
       </Typography>
       <Typography align={'center'} variant={'h5'}>
         Your contribution will help improve conduit!
@@ -15,12 +16,14 @@ const ContributeSection: FC = () => {
 
       <Box display="flex" mt={6} mb={1} justifyContent={'center'}>
         <Button
+          href={'/contribute'}
+          component={Link}
           endIcon={<PeopleOutlinedIcon />}
           color={'secondary'}
           variant={'contained'}
           size={'large'}>
           <Typography align={'center'}>
-            <strong>Join the conduit Community</strong>
+            <strong>Join the Community</strong>
           </Typography>
         </Button>
       </Box>
