050733a5aff7564f550c99f90e9a3adf42f390d2
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-01-19 15:54:53 +0200

    fix(contribute): added more content and styling

--- www/pages/contribute.tsx
+++ www/pages/contribute.tsx
@@ -1,15 +1,19 @@
-import { Box, Container, Divider, Grid, Paper, Typography } from '@mui/material';
+import { Box, Container, Paper, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
 
 const ContributePaperComponent = styled(Paper)(({ theme }) => ({
   display: 'flex',
-  justifyContent: 'center',
+  flexWrap: 'wrap',
+  justifyContent: 'space-between',
   padding: theme.spacing(2),
   minHeight: '140px',
   borderRadius: '8px',
   borderColor: theme.palette.secondary.main,
   backgroundColor: 'rgba(0, 0, 0, 0.2)',
+  '& > *': {
+    flex: '1 1 50%',
+  },
 }));
 
 export default function Contribute() {
@@ -20,15 +24,52 @@ export default function Contribute() {
       </Typography>
       <ContributePaperComponent elevation={1} style={{ border: '1px solid black' }}>
         <Box>
-          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
+          <Typography padding="20px" variant="h5">
+            Conduit + Community = 💖
+          </Typography>
+          <Typography padding="20px" variant="subtitle2">
+            We strongly believe that the community around Conduit can really make the difference. No
+            matter how experienced you are, if you like the idea behind the project and would like
+            express your ideas via your code, simplifying the backend process for you and other
+            Conduit members, you are more than welcome to join us!
+          </Typography>
         </Box>
-        <Divider style={{ backgroundColor: '#07D9C4' }} orientation="vertical" flexItem />
-        <Box>
-          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
-          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
-          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
-          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
-          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
+        <Box sx={{ borderLeft: (theme) => `1px solid ${theme.palette.secondary.main}` }}>
+          <Typography sx={{ padding: '20px' }} variant="h5">
+            Welcoming environment
+          </Typography>
+          <Typography padding="20px" variant="subtitle2">
+            In the interest of fostering an open and welcoming environment, we as contributors and
+            maintainers pledge to making participation in our project and our community a
+            harassment-free experience for everyone.
+          </Typography>
+          <Typography sx={{ padding: '20px' }} variant="h5">
+            It’s empowering to be able to make changes, even small ones
+          </Typography>
+          <Typography padding="20px" variant="subtitle2">
+            You don’t have to become a lifelong contributor to enjoy participating in open source.
+            Have you ever seen a typo on a website, and wished someone would fix it? On an open
+            source project, you can do just that. Open source helps people feel agency over their
+            lives and how they experience the world, and that in itself is gratifying.
+          </Typography>
+          <Typography style={{ marginBottom: '-30px' }} padding="20px" variant="h5">
+            <a href="https://github.com/Quintessential-SFT/conduit/blob/master/CODE_OF_CONDUCT.md">
+              Read our code of conduct ✊🏼
+            </a>
+          </Typography>
+          <Typography padding="20px" variant="subtitle2">
+            Also refer to our{' '}
+            <a href="https://github.com/Quintessential-SFT/conduit#readme">readme</a>
+          </Typography>
+          <Typography padding="20px" variant="h5">
+            Join us on social platforms!
+          </Typography>
+          <Typography padding="20px" variant="subtitle2">
+            <ul style={{ marginTop: '-10px' }}>
+              <li>Matrix</li>
+              <li>Discord</li>
+            </ul>
+          </Typography>
         </Box>
       </ContributePaperComponent>
     </Container>
