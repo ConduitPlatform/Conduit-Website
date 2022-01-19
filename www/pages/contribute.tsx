635231daa711fe86ad855b2ba7214dca593daa8e
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-01-19 14:50:33 +0200

    fix(contributePage): styling

--- www/pages/contribute.tsx
+++ www/pages/contribute.tsx
@@ -1,24 +1,35 @@
-import { Container, Paper, Typography } from '@mui/material';
+import { Box, Container, Divider, Grid, Paper, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
 
 const ContributePaperComponent = styled(Paper)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
   padding: theme.spacing(2),
   minHeight: '140px',
-  borderRadius: '12px',
+  borderRadius: '8px',
   borderColor: theme.palette.secondary.main,
-  flexDirection: 'column',
   backgroundColor: 'rgba(0, 0, 0, 0.2)',
 }));
 
 export default function Contribute() {
   return (
-    <Container maxWidth={'xl'} sx={{ pt: [4, 8, 12] }}>
+    <Container maxWidth={'lg'} sx={{ pt: [4, 8, 12] }}>
       <Typography textAlign="center" variant="h4" padding="25px">
         Contribute to Conduit
       </Typography>
-      <ContributePaperComponent elevation={10} style={{ border: '1px solid black' }}>
-        <Typography>We contribute here</Typography>
+      <ContributePaperComponent elevation={1} style={{ border: '1px solid black' }}>
+        <Box>
+          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
+        </Box>
+        <Divider style={{ backgroundColor: '#07D9C4' }} orientation="vertical" flexItem />
+        <Box>
+          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
+          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
+          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
+          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
+          <Typography style={{ padding: '20px' }}>We contribute here</Typography>
+        </Box>
       </ContributePaperComponent>
     </Container>
   );
