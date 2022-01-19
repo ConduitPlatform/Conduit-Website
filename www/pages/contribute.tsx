119472f57a0966f158e9bb4a808c58a057ba65a2
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-01-19 16:21:56 +0200

    fix(contribute): Typography component to reduce repetition

--- www/pages/contribute.tsx
+++ www/pages/contribute.tsx
@@ -25,6 +25,10 @@ const Heading = styled(Typography)(({ theme }) => ({
   textShadow: `1px 2px ${theme.palette.secondary.main}`,
 }));
 
+const TypographyComponent = styled(Typography)(({ theme }) => ({
+  padding: '18px',
+}));
+
 export default function Contribute() {
   return (
     <Container maxWidth={'lg'} sx={{ pt: [4, 8, 12] }}>
@@ -33,57 +37,51 @@ export default function Contribute() {
       </Heading>
       <ContributePaperComponent elevation={1} style={{ border: '1px solid black' }}>
         <Box>
-          <Typography padding="20px" variant="h5">
-            Conduit + Community = 💖
-          </Typography>
-          <Typography padding="20px" variant="subtitle2">
+          <TypographyComponent variant="h5">Conduit + Community = 💖</TypographyComponent>
+          <TypographyComponent variant="subtitle2">
             We strongly believe that the community around Conduit can really make the difference. No
             matter how experienced you are, if you like the idea behind the project and would like
             express your ideas via your code, simplifying the backend process for you and other
             Conduit members, you are more than welcome to join us!
-          </Typography>
+          </TypographyComponent>
         </Box>
         <Box
           sx={{
             borderLeft: (theme) => ['none', `1px solid ${theme.palette.secondary.main}`],
           }}>
-          <Typography sx={{ padding: '20px' }} variant="h5">
-            Welcoming environment
-          </Typography>
-          <Typography padding="20px" variant="subtitle2">
+          <TypographyComponent variant="h5">Welcoming environment</TypographyComponent>
+          <TypographyComponent variant="subtitle2">
             In the interest of fostering an open and welcoming environment, we as contributors and
             maintainers pledge to making participation in our project and our community a
             harassment-free experience for everyone.
-          </Typography>
-          <Typography sx={{ padding: '20px' }} variant="h5">
+          </TypographyComponent>
+          <TypographyComponent variant="h5">
             It’s empowering to be able to make changes, even small ones
-          </Typography>
-          <Typography padding="20px" variant="subtitle2">
+          </TypographyComponent>
+          <TypographyComponent variant="subtitle2">
             You don’t have to become a lifelong contributor to enjoy participating in open source.
             Have you ever seen a typo on a website, and wished someone would fix it? On an open
             source project, you can do just that. Open source helps people feel agency over their
             lives and how they experience the world, and that in itself is gratifying.
-          </Typography>
-          <Typography style={{ marginBottom: '-30px' }} padding="20px" variant="h5">
+          </TypographyComponent>
+          <TypographyComponent style={{ marginBottom: '-30px' }} padding="20px" variant="h5">
             <ATagComponent href="https://github.com/Quintessential-SFT/conduit/blob/master/CODE_OF_CONDUCT.md">
               Read our code of conduct ✊🏼
             </ATagComponent>
-          </Typography>
-          <Typography padding="20px" variant="subtitle2">
+          </TypographyComponent>
+          <TypographyComponent variant="subtitle2">
             Also refer to our{' '}
             <ATagComponent href="https://github.com/Quintessential-SFT/conduit#readme">
               readme
             </ATagComponent>
-          </Typography>
-          <Typography padding="20px" variant="h5">
-            Join us on social platforms!
-          </Typography>
-          <Typography padding="20px" variant="subtitle2">
+          </TypographyComponent>
+          <TypographyComponent variant="h5">Join us on social platforms!</TypographyComponent>
+          <TypographyComponent variant="subtitle2">
             <ul style={{ marginTop: '-10px' }}>
               <li>Matrix</li>
               <li>Discord</li>
             </ul>
-          </Typography>
+          </TypographyComponent>
         </Box>
       </ContributePaperComponent>
     </Container>
