96eed27deff0bdac38166fc4e5388da30f1549ad
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-01-31 15:33:49 +0200

    fix(roadmap): customized

--- www/pages/roadmap.tsx
+++ www/pages/roadmap.tsx
@@ -6,52 +6,66 @@ import Documentation from '../public/timelineIcons/documentation.svg';
 import Performance from '../public/timelineIcons/performance.svg';
 import 'react-vertical-timeline-component/style.min.css';
 import { Container } from '@mui/material';
+import { useTheme } from '@mui/system';
 
 export default function Roadmap() {
+  const theme = useTheme();
   return (
     <Container maxWidth="md">
-      <VerticalTimeline lineColor="#5B44F2">
+      <VerticalTimeline lineColor={theme.palette.secondary.dark}>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: '#07D9C4', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-          date="2011 - present"
-          iconStyle={{ background: '#262840', color: '#5B44F2' }}
+          contentStyle={{
+            background: 'transparent',
+            color: '#fff',
+            boxShadow: `0px 0px 0px 1px ${theme.palette.text.disabled}, 0 3px 0  ${theme.palette.text.primary}`,
+          }}
+          iconStyle={{
+            background: '#262840',
+            color: '#5B44F2',
+            transform: 'scale(0.7)',
+            backgroundColor: theme.palette.secondary.dark,
+          }}
           icon={<TypeScript />}>
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <h3 className="vertical-timeline-element-title">Typescript</h3>
           <p>
             Creative Direction, User Experience, Visual Design, Project Management, Team Leading
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2010 - 2011"
-          contentStyle={{ background: '#07D9C4', color: '#fff' }}
-          iconStyle={{ background: '#262840', color: '#5B44F2' }}
+          contentStyle={{
+            background: 'transparent',
+            color: '#fff',
+            boxShadow: `0px 0px 0px 1px ${theme.palette.text.disabled}, 0 3px 0  ${theme.palette.text.primary}`,
+          }}
+          iconStyle={{ background: '#262840', color: '#5B44F2', transform: 'scale(0.7)' }}
           icon={<NewFeatures />}>
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <h3 className="vertical-timeline-element-title">New features</h3>
           <p>Creative Direction, User Experience, Visual Design, SEO, Online Marketing</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2010 - 2011"
-          contentStyle={{ background: '#07D9C4', color: '#fff' }}
-          iconStyle={{ background: '#262840', color: '#5B44F2' }}
+          contentStyle={{
+            background: 'transparent',
+            color: '#fff',
+            boxShadow: `0px 0px 0px 1px ${theme.palette.text.disabled}, 0 3px 0  ${theme.palette.text.primary}`,
+          }}
+          iconStyle={{ background: '#262840', color: '#5B44F2', transform: 'scale(0.7)' }}
           icon={<Documentation />}>
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <h3 className="vertical-timeline-element-title">Documentation</h3>
           <p>Creative Direction, User Experience, Visual Design, SEO, Online Marketing</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2008 - 2010"
-          contentStyle={{ background: '#07D9C4', color: '#fff' }}
-          iconStyle={{ background: '#262840', color: '#5B44F2' }}
+          contentStyle={{
+            background: 'transparent',
+            color: '#fff',
+            boxShadow: `0px 0px 0px 1px ${theme.palette.text.disabled}, 0 3px 0  ${theme.palette.text.primary}`,
+          }}
+          iconStyle={{ background: '#262840', color: '#5B44F2', transform: 'scale(0.7)' }}
           icon={<Performance />}>
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+          <h3 className="vertical-timeline-element-title">Performance</h3>
           <p>User Experience, Visual Design</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
