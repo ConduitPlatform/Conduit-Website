abfb417bbcab782f7f72868d96add7f28672bb03
jasonkatsoutas <jasonskate33@gmail.com>
2022-01-31 17:44:56 +0200

    fix: module section padding fix

--- www/src/components/sections/ModulesSection.tsx
+++ www/src/components/sections/ModulesSection.tsx
@@ -8,14 +8,14 @@ import CustomSnapBox from '../custom/CustomSnapBox';
 export default function ModulesSection() {
   return (
     <Box mt={[12, 16]}>
-      <Box py={3}>
+      <Box py={3} px={[2, 2, 3]}>
         <Typography mb={1} alignItems={'center'} display={'flex'} variant={'h4'}>
           <ViewModuleTwoTone sx={{ width: 40, height: 40, mr: 1 }} color={'secondary'} />
           <strong> Build a backend in minutes</strong>
         </Typography>
         <Typography variant={'body1'}>with various modules Conduit has to offer!</Typography>
       </Box>
-      <CustomSnapBox gridColumnLength={ModuleCardData.length} pb={1} gap={3}>
+      <CustomSnapBox gridColumnLength={ModuleCardData.length} pb={1} px={[0, 0, 3]} gap={3}>
         {ModuleCardData.map((item, index) => (
           <Box key={index}>
             <ModuleCard
