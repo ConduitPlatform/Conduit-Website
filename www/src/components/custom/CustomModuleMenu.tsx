af297469833734448525f5f0fa99f620f7caa2f1
jasonkatsoutas <jasonskate33@gmail.com>
2022-02-01 12:43:15 +0200

    fix: module menu styling

--- www/src/components/custom/CustomModuleMenu.tsx
+++ www/src/components/custom/CustomModuleMenu.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { Button } from '@mui/material';
-import { alpha, styled } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Menu, { MenuProps } from '@mui/material/Menu';
 import * as React from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
@@ -11,39 +11,22 @@ const StyledMenu = styled((props: MenuProps) => (
     elevation={6}
     anchorOrigin={{
       vertical: 'bottom',
-      horizontal: 'left',
+      horizontal: 'center',
     }}
     transformOrigin={{
       vertical: 'top',
-      horizontal: 'left',
+      horizontal: 'center',
     }}
     {...props}
   />
 ))(({ theme }) => ({
   '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
-      },
-    },
-    '& a': {
-      textDecoration: 'none',
-      color: theme.palette.text.secondary,
-      display: 'flex',
-      alignItems: 'center',
-    },
+    background: `linear-gradient(180deg, ${theme.palette.background.paper} 27%, ${theme.palette.background.default} 89%)`,
+    width: '50%',
+  },
+  '& .MuiList-root': {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
   },
 }));
 
@@ -64,6 +47,7 @@ const CustomModuleMenu: FC = () => {
         MODULES
       </Button>
       <StyledMenu anchorEl={anchorEl} open={!!anchorEl} onClose={handleClose}>
+        <CustomMenuItem>CMS</CustomMenuItem>
         <CustomMenuItem>CMS</CustomMenuItem>
         <CustomMenuItem>MAILS</CustomMenuItem>
         <CustomMenuItem>STORAGE</CustomMenuItem>
