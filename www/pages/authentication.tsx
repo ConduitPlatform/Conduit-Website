0468074c17e718f0f2298cd4bd07c18ea959963f
jasonkatsoutas <jasonskate33@gmail.com>
2022-02-02 13:56:40 +0200

    fix: authentication page content

--- www/pages/authentication.tsx
+++ www/pages/authentication.tsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import ModulePage from '../src/components/custom/ModulePage';
-import Google from '../public/icons/google.svg';
 import Security from '../public/icons/security.svg';
-import LockIcon from '../public/icons/lock.svg';
-import TwoFA from '../public/icons/2fa.svg';
+import KeyIcon from '../public/moduleIcons/key.svg';
+import SignIcon from '../public/moduleIcons/signIn.svg';
+
+import TwoFA from '../public/moduleIcons/2fa.svg';
 
 const Authentication = () => {
   return (
     <ModulePage
       moduleName="Authentication"
-      title="User authentication."
+      title="This module offers user authentication.
+It supports logins with local credentials as well as popular identity providers.
+TwoFactor authentication is also provided."
       docsLink="/docs/modules/storage"
       img={<Security />}
       features={[
         {
-          title: 'Local authentication ',
-          description: '',
+          title: 'Local authentication',
+          description:
+            'Local authentication with username/password or email/password, optional email verification, Forgot/Reset password and more!',
           url: '123',
-          icon: <LockIcon />,
+          icon: <KeyIcon />,
         },
         {
-          title: 'Popular identity providers',
-          description: '',
+          title: 'Sign in methods',
+          description:
+            'Login with client credentials from Facebook, Google, Kakao, Twitch. There are more to come!',
           url: '123',
-          icon: <Google />,
+          icon: <SignIcon />,
         },
         {
-          title: 'TwoFactor authentication ',
-          description: '',
+          title: '2FA',
+          description:
+            'Optional two factor authentication supported on all strategies (requires SMS module)',
           url: '123',
           icon: <TwoFA />,
         },
