30b90e255b6115ba2d2731a65c68e8944132fdae
ssellas <sellas.spy@gmail.com>
2022-02-03 11:00:11 +0200

    feat: contribute page additions

--- www/pages/authentication.tsx
+++ www/pages/authentication.tsx
@@ -13,28 +13,28 @@ const Authentication = () => {
       title="This module offers user authentication.
 It supports logins with local credentials as well as popular identity providers.
 TwoFactor authentication is also provided."
-      docsLink="/docs/modules/authentication/"
+      docsLink="/docs/modules/authentication"
       img={<Security />}
       features={[
         {
           title: 'Local authentication',
           description:
             'Local authentication with username/password or email/password, optional email verification, Forgot/Reset password and more!',
-          url: '/docs/modules/authentication/',
+          url: '/docs/modules/authentication/config',
           icon: <KeyIcon />,
         },
         {
           title: 'Sign in methods',
           description:
             'Login with client credentials from Facebook, Google, Kakao, Twitch. There are more to come!',
-          url: '/docs/modules/authentication/',
+          url: '/docs/modules/authentication/config#third-party',
           icon: <SignIcon />,
         },
         {
           title: '2FA',
           description:
             'Optional two factor authentication supported on all strategies (requires SMS module)',
-          url: '/docs/modules/authentication/',
+          url: '/docs/modules/authentication/config#how-to-enable-2fa',
           icon: <TwoFA />,
         },
       ]}
