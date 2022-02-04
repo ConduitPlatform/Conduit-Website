9c3cbb5bd89c2f40c26e53b2a9e7e3a5a5c1fc9e
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-02-04 15:17:49 +0200

    fix(docusaurusSEO): added global SEO to docs

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,116 +1,129 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Conduit Platform',
-  tagline: 'Less is more',
-  url: 'https://mystifying-borg-373afd.netlify.app',
-  baseUrl: '/docs/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'ConduitPlatform', // Usually your GitHub org/user name.
-  projectName: 'Conduit', // Usually your repo name.
+  title: "Conduit Platform",
+  tagline: "Less is more",
+  url: "https://mystifying-borg-373afd.netlify.app",
+  baseUrl: "/docs/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "ConduitPlatform", // Usually your GitHub org/user name.
+  projectName: "Conduit", // Usually your repo name.
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/ConduitPlatform/Conduit-Website/tree/master/documentation',
+          editUrl:
+            "https://github.com/ConduitPlatform/Conduit-Website/tree/master/documentation",
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
   themeConfig:
-  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      metadata: [
+        {
+          name: "title",
+          content: "Conduit | Documentation",
+        },
+        { name: "canonical", content: "https://www.getconduit.dev" },
+        { name: "image", content: "https://getconduit.dev/og/branding.png" },
+        { name: "description", content: "Learn about Conduit" },
+        { name: "og:title ", content: "Conduit | Documentation" },
+        { name: "og:description", content: "Learn about Conduit" },
+        { name: "og:image", content: "https://getconduit.dev/og/branding.png" },
+      ],
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
       },
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-          href: 'https://www.getconduit.dev',
-          target: '_self'
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+          href: "https://www.getconduit.dev",
+          target: "_self",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
           {
-            to: 'https://www.getconduit.dev/blog',
+            to: "https://www.getconduit.dev/blog",
             target: "_self",
-            label: 'Blog',
-            position: 'left'
+            label: "Blog",
+            position: "left",
           },
           {
-            href: 'https://github.com/ConduitPlatform/Conduit',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/ConduitPlatform/Conduit",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Overview',
-                to: '/',
+                label: "Overview",
+                to: "/",
               },
               {
-                label: 'Modules',
-                to: '/modules',
+                label: "Modules",
+                to: "/modules",
               },
               {
-                label: 'Admin Panel',
-                to: '/admin-panel',
+                label: "Admin Panel",
+                to: "/admin-panel",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/ConduitPlatform/Conduit',
+                label: "GitHub",
+                href: "https://github.com/ConduitPlatform/Conduit",
               },
             ],
           },
