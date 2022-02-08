2f79dafdf6efa896aededa58b0df484a80376be8
ssellas <sellas.spy@gmail.com>
2022-02-08 14:22:32 +0200

    feat: intro section syntax highlighter code, styling, copy button feat: netlify bootstrap redirect

--- www/src/components/sections/IntroSection.tsx
+++ www/src/components/sections/IntroSection.tsx
@@ -3,18 +3,35 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TypewriterComponent from 'typewriter-effect';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { duotoneLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import { Button, Grid, Grow } from '@mui/material';
+import { duotoneLight, duotoneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { Button, Grid, Grow, IconButton, Popover, useMediaQuery } from '@mui/material';
 import Illustration from '../../../public/icons/Illustration';
 import { styled } from '@mui/material/styles';
-import { ArrowForwardIos } from '@mui/icons-material';
+import { ArrowForwardIos, ContentCopy } from '@mui/icons-material';
 import Link from '../../Link';
 import { SxObject } from '../../models/SxObjects';
+import { useTheme } from '@mui/system';
 
 const styles: SxObject = {
   basicText: {
     textAlign: ['center', 'center', 'left'],
   },
+  highlighterContainer: {
+    flexGrow: '1',
+    flexShrink: '0',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  copyIcon: { marginLeft: 1 },
+};
+
+const highlighterCustomStyle = {
+  borderRadius: '4px',
+  padding: '0.8em',
+  boxShadow:
+    '0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)',
 };
 
 const StyledBox = styled(Box)(() => ({
@@ -25,7 +42,30 @@ const StyledBox = styled(Box)(() => ({
   gap: 20,
 }));
 
+const bootstrapBash = 'source <(curl -L -s https://getconduit.dev/bootstrap)';
+
 export default function IntroSection() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
+
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  const copyBash = (event: React.MouseEvent<HTMLButtonElement>) =>
+    (async () => {
+      const target = event.currentTarget;
+      await navigator.clipboard.writeText(bootstrapBash);
+      if (!open) {
+        setAnchorEl(target);
+        setTimeout(() => setAnchorEl(null), 2000);
+      }
+    })();
+
   return (
     <Box>
       <Grid container>
@@ -70,13 +110,38 @@ export default function IntroSection() {
               GET STARTED
             </Button>
 
-            <Box style={{ flexGrow: '1' }}>
-              <SyntaxHighlighter
-                style={duotoneLight}
-                customStyle={{ borderRadius: '4px', padding: '0.86em' }}>
-                {'curl .... docker compose'}
-              </SyntaxHighlighter>
-            </Box>
+            {matches ? (
+              <Box sx={styles.highlighterContainer}>
+                <SyntaxHighlighter
+                  language={'bash'}
+                  style={theme.palette.mode === 'light' ? duotoneDark : duotoneLight}
+                  customStyle={highlighterCustomStyle}>
+                  {bootstrapBash}
+                </SyntaxHighlighter>
+                <IconButton
+                  size={'small'}
+                  sx={styles.copyIcon}
+                  color={'secondary'}
+                  onClick={copyBash}>
+                  <ContentCopy fontSize={'small'} />
+                </IconButton>
+                <Popover
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  hideBackdrop
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}>
+                  <Typography sx={{ p: 1 }}>Copied!</Typography>
+                </Popover>
+              </Box>
+            ) : null}
           </StyledBox>
         </Grid>
         <Grid item md={6} sm={12} xs={12}>
