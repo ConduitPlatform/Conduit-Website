4072972c00c45b2b153507daf0fb5dd34c485b7e
kkopanidis <kkopanidis@gmail.com>
2022-03-06 17:35:22 +0200

    refactor(documentation): make overview a section add(documentation): new intro doc add(documentation): new features doc add(documentation): new quick start doc add(documentation): new architecture doc

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
         items: [
           {
             type: "doc",
-            docId: "intro",
+            docId: "overview/intro",
             position: "left",
             label: "Docs",
           },
