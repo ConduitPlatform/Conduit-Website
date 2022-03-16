fdd10cd9dcb2fcbdc699fe9c015c05bf83fae880
Konstantinos Feretos <konferetos@tutanota.com>
2022-03-16 18:01:13 +0200

    chore(documentantion): remove edit links (#80)

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -24,8 +24,7 @@ const config = {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/ConduitPlatform/Conduit-Website/tree/master/documentation",
+          editUrl: undefined,
         },
         blog: false,
         theme: {
