ae2389711d8b1f8395d0a5b4238c5f1c705d73ce
Konstantinos Feretos <konferetos@tutanota.com>
2022-12-09 15:29:45 +0200

    style(www): criminal-tier prettier formatting

--- www/src/components/custom/DownloadDialog/PlatformInstallation.tsx
+++ www/src/components/custom/DownloadDialog/PlatformInstallation.tsx
@@ -1,10 +1,10 @@
-import React, {FC, ReactNode} from 'react';
-import {Box, IconButton, Popover, Typography, useMediaQuery, useTheme} from '@mui/material';
-import {Platform} from './types';
-import {highlighterCustomStyle, styles} from './HighlighterStyles';
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
-import {dracula} from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import {ContentCopy} from '@mui/icons-material';
+import React, { FC, ReactNode } from 'react';
+import { Box, IconButton, Popover, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Platform } from './types';
+import { highlighterCustomStyle, styles } from './HighlighterStyles';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { ContentCopy } from '@mui/icons-material';
 
 type PlatformInstallInfo = {
   installLines: [string, string?][];
@@ -23,24 +23,45 @@ const PlatformInstallation: FC<Props> = ({ platform }) => {
   const [copyBtnElement, setCopyBtnElement] = React.useState<HTMLButtonElement | null>(null);
 
   const platformInfo: Map<Platform, PlatformInstallInfo> = new Map([
-    [Platform.LINUX, {
-      installLines: [['sh <(curl -s https://getconduit.dev/bootstrap)', cliInstallText]],
-    }],
-    [Platform.MAC, {
-      installLines: [['sh <(curl -s https://getconduit.dev/bootstrap)', cliInstallText]],
-    }],
-    [Platform.NPM, {
-      installLines: [['npx -y @conduitplatform/cli@latest deploy setup --config', cliInstallText]],
-      bottomText: <strong hidden={window.navigator.platform !== 'Win32'}>Windows users should run through WSL</strong>
-    }],
-    [Platform.HELM, {
-      installLines: [
-        ['helm repo add conduit-platform https://conduitplatform.github.io/helm-charts', 'Add Helm Repo'],
-        ['helm repo update'],
-        ['helm install conduit conduit-platform/conduit', 'Install Chart'],
-        ['kubectl port-forward svc/conduit-admin 8000:80', 'Expose Admin Dashboard'],
-      ],
-    }],
+    [
+      Platform.LINUX,
+      {
+        installLines: [['sh <(curl -s https://getconduit.dev/bootstrap)', cliInstallText]],
+      },
+    ],
+    [
+      Platform.MAC,
+      {
+        installLines: [['sh <(curl -s https://getconduit.dev/bootstrap)', cliInstallText]],
+      },
+    ],
+    [
+      Platform.NPM,
+      {
+        installLines: [
+          ['npx -y @conduitplatform/cli@latest deploy setup --config', cliInstallText],
+        ],
+        bottomText: (
+          <strong hidden={window.navigator.platform !== 'Win32'}>
+            Windows users should run through WSL
+          </strong>
+        ),
+      },
+    ],
+    [
+      Platform.HELM,
+      {
+        installLines: [
+          [
+            'helm repo add conduit-platform https://conduitplatform.github.io/helm-charts',
+            'Add Helm Repo',
+          ],
+          ['helm repo update'],
+          ['helm install conduit conduit-platform/conduit', 'Install Chart'],
+          ['kubectl port-forward svc/conduit-admin 8000:80', 'Expose Admin Dashboard'],
+        ],
+      },
+    ],
   ]);
 
   const copyLine = async (text: string, btnElement: HTMLButtonElement) => {
@@ -56,13 +77,17 @@ const PlatformInstallation: FC<Props> = ({ platform }) => {
   const activeInfo = platformInfo.get(platform) ?? platformInfo.get(Platform.NPM)!;
   const lineElements: React.ReactElement[] = [];
   activeInfo.installLines.forEach((line, index) => {
-    const fontSize: { desktop: string, mobile: string } = {
+    const fontSize: { desktop: string; mobile: string } = {
       desktop: platform === Platform.HELM ? '0.7em' : '0.9em',
       mobile: platform === Platform.HELM ? '0.6em' : '0.7em',
     };
     lineElements.push(
       <Box display="flex" flexDirection="column">
-        { line[1] && <Typography textAlign="center" variant="caption" marginTop={2}>{line[1]}</Typography> }
+        {line[1] && (
+          <Typography textAlign="center" variant="caption" marginTop={2}>
+            {line[1]}
+          </Typography>
+        )}
         <Box sx={styles.highlighterContainer}>
           <SyntaxHighlighter
             language={'bash'}
@@ -78,31 +103,30 @@ const PlatformInstallation: FC<Props> = ({ platform }) => {
           </SyntaxHighlighter>
           {navigator.clipboard && (
             <>
-            <IconButton
-              id={`btn-${index}`}
-              size={'small'}
-              sx={styles.copyIcon}
-              color={'secondary'}
-              onClick={(event) => copyLine(line[0], event.currentTarget)}
-            >
-              <ContentCopy fontSize={'small'} />
-            </IconButton>
-            <Popover
-              id={`popover-${index}`}
-              open={copyBtnElement?.id.split('-')[1] === String(index)}
-              anchorEl={copyBtnElement}
-              onClose={() => setCopyBtnElement(null)}
-              hideBackdrop
-              anchorOrigin={{
-                vertical: 'center',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'center',
-                horizontal: 'left',
-              }}>
-              <Typography sx={{ p: 1 }}>Copied!</Typography>
-            </Popover>
+              <IconButton
+                id={`btn-${index}`}
+                size={'small'}
+                sx={styles.copyIcon}
+                color={'secondary'}
+                onClick={(event) => copyLine(line[0], event.currentTarget)}>
+                <ContentCopy fontSize={'small'} />
+              </IconButton>
+              <Popover
+                id={`popover-${index}`}
+                open={copyBtnElement?.id.split('-')[1] === String(index)}
+                anchorEl={copyBtnElement}
+                onClose={() => setCopyBtnElement(null)}
+                hideBackdrop
+                anchorOrigin={{
+                  vertical: 'center',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'center',
+                  horizontal: 'left',
+                }}>
+                <Typography sx={{ p: 1 }}>Copied!</Typography>
+              </Popover>
             </>
           )}
         </Box>
@@ -114,7 +138,9 @@ const PlatformInstallation: FC<Props> = ({ platform }) => {
     <Box display="flex" alignItems="center" flexDirection="column" gap={2}>
       <Box display="flex" flexDirection="column">
         {lineElements}
-        {activeInfo.bottomText && <Typography textAlign="center">{activeInfo.bottomText}</Typography>}
+        {activeInfo.bottomText && (
+          <Typography textAlign="center">{activeInfo.bottomText}</Typography>
+        )}
       </Box>
       {/*{platformInfo.get(platform)!.bottomText && <Typography textAlign="center" variant="caption">{platformInfo.get(platform)!.bottomText]}</Typography> }*/}
     </Box>
